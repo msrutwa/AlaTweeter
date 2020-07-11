20f7afd91ab8148c12330b46135093a685dbe87a
Małgorzata Śrutwa <gos@MacBook-Pro-Magorzata.local>
2020-07-11 16:02:39 +0200

    AT-1. HibernateUtil, AppUser, Implemented, Database configuration added.

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import model.AppUser;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<AppUser> getAll();
+
+    void saveUser (AppUser user);
+    void deleteUser(long id);
+    AppUser getUserByEmail(String email);
+
+    AppUser getUserByLogin(String login);
+
+    List<AppUser> getUsersByName(String name);
+
+    boolean isUserValid (String login, String password);
+
+    List<AppUser> getFollowedUsers(String ligin);
+
+    List<AppUser> getFollowers(String login);
+
+    void follow(String currentUserLogin, String userLoginToFollow);
+
+    void stopFollowing(String currentUserLogin, String userLoginToStopFollow);
+
+
+
+
+}
